635283202acd1c3d667bae852f62256af683161c
Emmanuel Pilande <epilande@gmail.com>
2019-08-04 02:38:00 -0700

    :construction: Update demo

--- demo/src/pages/index.tsx
+++ demo/src/pages/index.tsx
@@ -1,13 +1,36 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import { auth, useAuth } from "gatsby-theme-firebase";
 import { Layout } from "../components";
 
 const HomePage = () => {
+  const { isLoading, isLoggedIn, profile } = useAuth();
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <Layout>
       <h1>
         <pre>Hello, Demo Site</pre>
       </h1>
+      {isLoggedIn ? (
+        <React.Fragment>
+          <p>Hi {profile.displayName}</p>
+          <button
+            onClick={() => {
+              auth.signOut();
+            }}
+          >
+            Sign Out
+          </button>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <Link to="/login">Login</Link> <Link to="/sign-up">Sign Up</Link>
+        </React.Fragment>
+      )}
     </Layout>
   );
 };
