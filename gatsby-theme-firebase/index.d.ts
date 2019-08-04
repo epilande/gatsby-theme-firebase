85c2867765ee3e82aea8f5efbef285c395fba03a
Emmanuel Pilande <epilande@gmail.com>
2019-08-04 02:37:09 -0700

    :package: Export library

---
+++ gatsby-theme-firebase/index.d.ts
@@ -0,0 +1,2 @@
+export { default as firebase, auth, firestore } from "./src/firebase";
+export { default as useAuth } from "./src/hooks/useAuth";
--- gatsby-theme-firebase/index.js
+++ gatsby-theme-firebase/index.js
@@ -1,1 +1,2 @@
-// noop
+export { default as firebase, auth, firestore } from "./src/firebase";
+export { default as useAuth } from "./src/hooks/useAuth";
