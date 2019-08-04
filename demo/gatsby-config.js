36e6a6c15b6f5e0d36abc8bfe9b642b7956d3c6a
Emmanuel Pilande <epilande@gmail.com>
2019-08-04 01:10:04 -0700

    :wrench: Pass firebase envs

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -1,8 +1,26 @@
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development";
+console.log(`Using environment config: '${activeEnv}'`);
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+});
+
 module.exports = {
   plugins: [
     {
       resolve: "gatsby-theme-firebase",
-      options: {},
+      options: {
+        credentials: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.FIREBASE_DATABASE_URL,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID,
+        },
+      },
     },
     "gatsby-plugin-typescript",
     "gatsby-plugin-emotion",
--- gatsby-theme-firebase/gatsby-node.js
+++ gatsby-theme-firebase/gatsby-node.js
@@ -1,11 +1,41 @@
 const path = require("path");
 
-exports.createPages = ({ actions }, options) => {
-  const { createPage } = actions;
-  const { basePath } = options;
+const checkRequiredCreds = creds => {
+  Object.entries(creds).map(([key, value]) => {
+    if (!value) {
+      throw new Error(`Required option "${key}" not specified`);
+    }
+  });
+};
 
-  createPage({
-    path: basePath || "/",
-    component: path.resolve(`${__dirname}/src/pages/index.tsx`),
+exports.onCreateWebpackConfig = ({ plugins, actions }, options) => {
+  const {
+    credentials: {
+      apiKey,
+      authDomain,
+      databaseURL,
+      projectId,
+      storageBucket,
+      messagingSenderId,
+      appId,
+    },
+  } = options;
+
+  checkRequiredCreds({ ...options.credentials });
+
+  actions.setWebpackConfig({
+    plugins: [
+      plugins.define({
+        "process.env": {
+          FIREBASE_API_KEY: JSON.stringify(apiKey),
+          FIREBASE_AUTH_DOMAIN: JSON.stringify(authDomain),
+          FIREBASE_DATABASE_URL: JSON.stringify(databaseURL),
+          FIREBASE_PROJECT_ID: JSON.stringify(projectId),
+          FIREBASE_STORAGE_BUCKET: JSON.stringify(storageBucket),
+          FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(messagingSenderId),
+          FIREBASE_APP_ID: JSON.stringify(appId),
+        },
+      }),
+    ],
   });
 };
