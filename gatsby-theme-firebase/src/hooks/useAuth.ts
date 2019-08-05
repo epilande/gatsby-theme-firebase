ee70ed88bc860e5928af2fa8a4f88924e673e85b
Emmanuel Pilande <epilande@gmail.com>
2019-08-04 23:29:30 -0700

    :label: Add firebase user type

--- gatsby-theme-firebase/src/hooks/useAuth.ts
+++ gatsby-theme-firebase/src/hooks/useAuth.ts
@@ -1,10 +1,11 @@
 import * as React from "react";
+import firebase from "firebase";
 import { auth } from "../firebase";
 
 const useAuth = () => {
   const [isLoading, setIsLoading] = React.useState(true);
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-  const [profile, setProfile] = React.useState<any>(null);
+  const [profile, setProfile] = React.useState<firebase.User | null>(null);
 
   React.useEffect(() => {
     auth.onAuthStateChanged(user => {
