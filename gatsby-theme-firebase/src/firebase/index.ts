f6353401cee7fcdad13b04577d668e706d26ce67
Emmanuel Pilande <epilande@gmail.com>
2019-08-06 14:22:50 -0700

    :building_construction: Fix gatsby build; Fix Firebase auth issue

--- gatsby-theme-firebase/src/firebase/index.ts
+++ gatsby-theme-firebase/src/firebase/index.ts
@@ -3,11 +3,19 @@ import "firebase/auth";
 import "firebase/firestore";
 import config from "./config";
 
+const isBrowser = typeof window !== "undefined";
+
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
 }
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+let auth: firebase.auth.Auth;
+let firestore: firebase.firestore.Firestore;
 
+if (isBrowser) {
+  auth = firebase.auth();
+  firestore = firebase.firestore();
+}
+
+export { auth, firestore };
 export default firebase;
