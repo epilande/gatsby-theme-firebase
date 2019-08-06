87689c3a28b261f25d2cb276230b357365ab3d7d
Emmanuel Pilande <epilande@gmail.com>
2019-08-06 13:44:24 -0700

    :tada: Add LoginModal demo

---
+++ demo/src/components/LoginModal.tsx
@@ -0,0 +1,41 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { Form, FormState } from "gatsby-theme-firebase";
+
+const LoginModal: React.FunctionComponent<{
+  setToggleLogin: (arg: boolean) => void;
+}> = ({ setToggleLogin }) => (
+  <div>
+    <div
+      sx={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100vh",
+        background: "rgba(0,0,0,0.5)",
+      }}
+      onClick={() => {
+        setToggleLogin(false);
+      }}
+    />
+    <div
+      sx={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <FormState.Provider>
+        <Form
+          onSuccess={() => {
+            setToggleLogin(false);
+          }}
+        />
+      </FormState.Provider>
+    </div>
+  </div>
+);
+
+export default LoginModal;
--- demo/src/pages/index.tsx
+++ demo/src/pages/index.tsx
@@ -1,11 +1,14 @@
 /** @jsx jsx */
 import { jsx, ThemeProvider } from "theme-ui";
+import * as React from "react";
 import { Link } from "gatsby";
 import { auth, useAuth, theme } from "gatsby-theme-firebase";
 import Button from "gatsby-theme-firebase/src/components/Button";
 import { Layout, DisplayState } from "../components";
+import LoginModal from "../components/LoginModal";
 
 const HomePage = () => {
+  const [toggleLogin, setToggleLogin] = React.useState(false);
   const { isLoading, isLoggedIn, profile } = useAuth();
 
   if (isLoading) {
@@ -15,21 +18,41 @@ const HomePage = () => {
   return (
     <ThemeProvider theme={theme}>
       <Layout>
-        <h1 sx={{ mt: 2 }}>Hello, Demo Site</h1>
+        <div
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <h1 sx={{ mt: 2 }}>Hello, Demo Site</h1>
+          {isLoggedIn && (
+            <Button
+              onClick={() => {
+                auth.signOut();
+              }}
+            >
+              Sign Out
+            </Button>
+          )}
+        </div>
 
         <DisplayState props={profile} />
 
-        {isLoggedIn ? (
-          <Button
-            onClick={() => {
-              auth.signOut();
-            }}
-          >
-            Sign Out
-          </Button>
-        ) : (
-          <Link to="/login">Login</Link>
-        )}
+        <Button
+          sx={{ mr: 3 }}
+          onClick={() => {
+            setToggleLogin(true);
+          }}
+        >
+          Login Modal
+        </Button>
+
+        <Link to="/login">
+          <Button>Login Page</Button>
+        </Link>
+
+        {toggleLogin && <LoginModal setToggleLogin={setToggleLogin} />}
       </Layout>
     </ThemeProvider>
   );
