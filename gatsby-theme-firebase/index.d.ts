7e55a13f650ba1cf381004a8519d266731b32cbc
Emmanuel Pilande <epilande@gmail.com>
2019-08-06 01:33:38 -0700

    :package: Export theme & components

--- gatsby-theme-firebase/index.d.ts
+++ gatsby-theme-firebase/index.d.ts
@@ -10,4 +10,10 @@ export {
   googleProvider,
   twitterProvider,
 } from "./src/firebase/auth";
+
 export { default as useAuth } from "./src/hooks/useAuth";
+
+export { default as Form } from "./src/components/Form";
+export { default as FormState } from "./src/containers/FormState";
+
+export { default as theme } from "./src/gatsby-plugin-theme-ui";
