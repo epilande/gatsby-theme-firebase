ba52a6a22a3b7fe2672dfb3431346ab2d5abb2cc
Emmanuel Pilande <epilande@gmail.com>
2019-08-13 10:17:56 -0700

    :partly_sunny: Update handleLoginSuccess shadowing example

--- demo/src/gatsby-theme-firebase/firebase/auth/handleLoginSuccess.ts
+++ demo/src/gatsby-theme-firebase/firebase/auth/handleLoginSuccess.ts
@@ -1,17 +1,13 @@
 import { navigate } from "gatsby";
-import firebase from "firebase";
+import { LoginSuccessArgs } from "gatsby-theme-firebase/src/firebase/auth/handleLoginSuccess";
 
-export default ({
-  user,
-  loginRedirectPath,
-}: {
-  user?: firebase.auth.UserCredential;
-  loginRedirectPath: string;
-}) => {
-  console.info(
-    "Shadowing handleLoginSuccess! View example: https://github.com/epilande/gatsby-theme-firebase/blob/master/demo/src/gatsby-theme-firebase/firebase/auth/handleLoginSuccess.ts",
+export default ({ user, loginRedirectPath }: LoginSuccessArgs) => {
+  console.log(
+    "%cShadowing handleLoginSuccess!%c View example: https://github.com/epilande/gatsby-theme-firebase/blob/master/demo/src/gatsby-theme-firebase/firebase/auth/handleLoginSuccess.ts",
+    "font-weight: bold; color: #eee; background: #3273DC; padding: 2px 5px;",
+    "",
   );
-  console.info(
+  console.log(
     "Original handleLoginSuccess handler: https://github.com/epilande/gatsby-theme-firebase/blob/master/gatsby-theme-firebase/src/firebase/auth/handleLoginSuccess.ts",
   );
   console.log("User: ", user);
